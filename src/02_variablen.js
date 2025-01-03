23dd7c6d4a89210e04979489f8db927c79016431
Vladimir Haar <haar.vlad@gmx.de>
2025-01-03 09:50:15 +0000

    Wertzuweisung 2 + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,4 +1,4 @@
-console.log('Frohes Neues');    // Frohes Neues
+// console.log('Frohes Neues');    // Frohes Neues
 
 // Zeilenkommentar
 
@@ -11,16 +11,37 @@ Blockkommentar
  * Variablen 01
  */
 
-console.log("Hi");          // Hi
+// console.log("Hi");          // Hi
 // console.log(Hi); so würde es nicht funktionieren. Es fehlen klammern.
 
 /* Deklaration und Wertzuweisung  */
-let firstName;              // Deklaration einer Varible
-console.log(firstName);     // undefined
-firstName = "Max";          // Wertzuweisung
-console.log(firstName)      // Max
+// let firstName;              // Deklaration einer Varible
+// console.log(firstName);     // undefined
+// firstName = "Max";          // Wertzuweisung
+// console.log(firstName)      // Max
 
-let familyName = "Mütze";   // Variable deklariert und den Wert direkt Zugewiesen
+// let familyName = "Mütze";   // Variable deklariert und den Wert direkt Zugewiesen
 
-console.log("Hi, ich bin" + " " + firstName + " " + familyName + ".")       // Hi, ich bin Max Mütze.
+// console.log("Hi, ich bin" + " " + firstName + " " + familyName + ".")       // Hi, ich bin Max Mütze.
+
+/* Wertzuweisung 2 + Datentypen */
+let test;
+console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
+console.log("Inhalt: " + test);             // Inhalt: undefined
+console.log("-------------------");
+
+test = "hello";
+console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
+console.log("Inhalt: " + test);             // Inhalt: hello
+console.log("-------------------");
+
+test = 2;
+console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: number
+console.log("Inhalt: " + test);             // Inhalt: 2
+console.log("-------------------");
+
+test = true;
+console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: boolean
+console.log("Inhalt: " + test);             // Inhalt: true
+console.log("-------------------");
 
