8f2e5c28fa06a0e41284893e875c2d09bdb064d3
Vladimir Haar <haar.vlad@gmx.de>
2025-01-03 09:17:56 +0000

    02 / Deklaration und Wertezuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,1 +1,26 @@
-console.log('Frohes Neues');
+console.log('Frohes Neues');    // Frohes Neues
+
+// Zeilenkommentar
+
+/*
+Blockkommentar
+*/
+
+/**
+ * 
+ * Variablen 01
+ */
+
+console.log("Hi");          // Hi
+// console.log(Hi); so würde es nicht funktionieren. Es fehlen klammern.
+
+/* Deklaration und Wertzuweisung  */
+let firstName;              // Deklaration einer Varible
+console.log(firstName);     // undefined
+firstName = "Max";          // Wertzuweisung
+console.log(firstName)      // Max
+
+let familyName = "Mütze";   // Variable deklariert und den Wert direkt Zugewiesen
+
+console.log("Hi, ich bin" + " " + firstName + " " + familyName + ".")       // Hi, ich bin Max Mütze.
+
