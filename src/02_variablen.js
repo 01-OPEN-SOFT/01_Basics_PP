8956d334425f28203d08d8041791b2635c6a473e
Vladimir Haar <haar.vlad@gmx.de>
2025-01-03 10:09:13 +0000

    Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -25,23 +25,36 @@ Blockkommentar
 // console.log("Hi, ich bin" + " " + firstName + " " + familyName + ".")       // Hi, ich bin Max Mütze.
 
 /* Wertzuweisung 2 + Datentypen */
-let test;
-console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
-console.log("Inhalt: " + test);             // Inhalt: undefined
-console.log("-------------------");
+// let test;
+// console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
+// console.log("Inhalt: " + test);             // Inhalt: undefined
+// console.log("-------------------");
 
-test = "hello";
-console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
-console.log("Inhalt: " + test);             // Inhalt: hello
-console.log("-------------------");
+// test = "hello";
+// console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: undefined
+// console.log("Inhalt: " + test);             // Inhalt: hello
+// console.log("-------------------");
 
-test = 2;
-console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: number
-console.log("Inhalt: " + test);             // Inhalt: 2
-console.log("-------------------");
+// test = 2;
+// console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: number
+// console.log("Inhalt: " + test);             // Inhalt: 2
+// console.log("-------------------");
 
-test = true;
-console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: boolean
-console.log("Inhalt: " + test);             // Inhalt: true
-console.log("-------------------");
+// test = true;
+// console.log("Datentyp: " + typeof test);    // lassen wir uns ein Datentyp ausgeben. Datentyp: boolean
+// console.log("Inhalt: " + test);             // Inhalt: true
+// console.log("-------------------");
 
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test;                                      // Variable : Deklaration
+test = "hi";                                   // Wertzuweisung (Initialisierung)
+test = "hello";                                // Überschreiben
+console.log("Inhalt: " + test);                // Ausgabe: Inhalt: hello
+
+// Konstante
+const test2 = "Hi!";                            // Konstante : Deklaration und Zuweisung
+console.log("Inhalt: " + test2);                // Ausgabe: Inhalt: Hi!
+test2 = "Hiiiii!";                              // Zuweisung der Konstanten einen anderen Wert 
+console.log("Inhalt: " + test2);                // Fehlermeldung. Überschreiben nicht möglich.
