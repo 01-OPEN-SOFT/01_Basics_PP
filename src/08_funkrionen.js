aae2401e879900ebd06f8ed5027db8fe7517b1b3
Vladimir Haar <haar.vlad@gmx.de>
2025-01-08 11:26:36 +0000

    08 / 3. Funktion in einer Funktion

--- src/08_funkrionen.js
+++ src/08_funkrionen.js
@@ -1,49 +1,58 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
-// // Funktionsdeklaration
-// function test() {
-//   console.log('Hallo, Manuel!'); // Hallo, Manuel!
-// }
-
-// // Funktionsaufruf
+// Funktionsaufruf | call
 // test();
 
-// /***** Funktionen 02a *****/
-// // 2a. Parametrisierung + Datenübergabe von INNEN
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test() {
+  console.log('Hallo, Manuel!');
+}
 
-// function ausgabeNamen() {
-//   let firstName = 'Manuel';
-//   console.log(`Hallo, ${firstName}!`);
-//   console.log(`--------------------`);
-// }
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
 
-// ausgabeNamen(); // Hallo, Manuel!
+// ausgabeNamen();  // call
 
-// /***** Funktionen 02b *****/
-// // 2b. Parametrisierung + Datenübergabe von AUSSEN
+function ausgabeNamen() {
+  // interne Variable | What happens in VEGAS ...
+  let firstName = 'Vladimir';
+  console.log('Hallo, ' + firstName + '!');
+}
 
-// // Funktion(Parameter)
-// function ausgabeName2(firstName) {
-//   console.log(`Hallo, ${firstName}!`);
-// }
-// ausgabeName2('Falk'); // Argumente werden übergeben --> Daten für Übergabe
+//console.log(firstName); // Fehler : scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// ausgabeNamen2("Falk"); // Argument --> Daten für Parameter
+// ausgabeNamen2("Maria");
+// ausgabeNamen2("Reza");
+
+function ausgabeNamen2(firstName) {
+  // Parameter
+  console.log('Hallo, ' + firstName + '!');
+}
 
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
+
+// ausgabeNamenParams("Max","Mütze");
+
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamenParams(prompt("Vorname? :"),prompt("Name? :")); // Piping
+
 function ausgabeNamenParams(firstName, secondName) {
-  console.log(`Hallo, ${firstName} ${secondName}`);
+  console.log('Hallo, ' + firstName + ' ' + secondName + '!');
 }
-
-ausgabeNamenParams('Max', 'Mütze'); // Hallo, Max Mütze
-
-const prompt = require('prompt-sync')({ sigint: true });
-ausgabeNamenParams(prompt('Vorname? :'), prompt('Name? :')); // Piping
 
 /***** Funktionen 03a *****/
 // 03a. Vorbereitung -Trennen der Funktionalitäten
 // Postulat: one function = one job (uncle Bob)
 // SRP single responsibility principle
+
+// ausgabeNamenSRP("Max","Mütze");
 
 function ausgabeNamenSRP(firstName, secondName) {
   // 1. Funktionalität: string composing
@@ -53,3 +62,22 @@ function ausgabeNamenSRP(firstName, secondName) {
   // 2. Funktionalität: string output
   console.log(outputStr);
 }
+
+/***** Funktionen 03b *****/
+// 1. Funktionalität: string composing
+
+function getString(firstName, secondName) {
+  const GAP = ' ';
+  let outputStr = 'Hallo,' + GAP + firstName + GAP + secondName + '!';
+  return outputStr;
+}
+
+// 2. Funktionalität: string output
+// output("hi");
+// output(2);
+// output(true);
+function output(outputData) {
+  console.log(outputData);
+}
+
+output(getString('Max', 'Mütze'));
