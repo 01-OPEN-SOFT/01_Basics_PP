cb1a3034f10d15a53595c460c7bf51f521b90f0e
Vladimir Haar <haar.vlad@gmx.de>
2025-01-08 10:10:35 +0000

    08 / 2. Verarbeitung SRP

--- src/08_funkrionen.js
+++ src/08_funkrionen.js
@@ -1,10 +1,55 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
-// Funktionsdeklaration
-function test() {
-  console.log('Hallo, Manuel!');
+// // Funktionsdeklaration
+// function test() {
+//   console.log('Hallo, Manuel!'); // Hallo, Manuel!
+// }
+
+// // Funktionsaufruf
+// test();
+
+// /***** Funktionen 02a *****/
+// // 2a. Parametrisierung + Datenübergabe von INNEN
+
+// function ausgabeNamen() {
+//   let firstName = 'Manuel';
+//   console.log(`Hallo, ${firstName}!`);
+//   console.log(`--------------------`);
+// }
+
+// ausgabeNamen(); // Hallo, Manuel!
+
+// /***** Funktionen 02b *****/
+// // 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// // Funktion(Parameter)
+// function ausgabeName2(firstName) {
+//   console.log(`Hallo, ${firstName}!`);
+// }
+// ausgabeName2('Falk'); // Argumente werden übergeben --> Daten für Übergabe
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+function ausgabeNamenParams(firstName, secondName) {
+  console.log(`Hallo, ${firstName} ${secondName}`);
 }
 
-// Funktionsaufruf
-test();
+ausgabeNamenParams('Max', 'Mütze'); // Hallo, Max Mütze
+
+const prompt = require('prompt-sync')({ sigint: true });
+ausgabeNamenParams(prompt('Vorname? :'), prompt('Name? :')); // Piping
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+function ausgabeNamenSRP(firstName, secondName) {
+  // 1. Funktionalität: string composing
+  const GAP = ' ';
+  let outputStr = 'Hallo,' + GAP + firstName + GAP + secondName + '!';
+
+  // 2. Funktionalität: string output
+  console.log(outputStr);
+}
