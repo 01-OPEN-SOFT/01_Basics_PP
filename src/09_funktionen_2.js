62589a6321c9ef017092e61a0cc16c184fa83b08
Vladimir Haar <haar.vlad@gmx.de>
2025-01-09 08:35:41 +0000

    Module + - * hinzugefügt

--- src/09_funktionen_2.js
+++ src/09_funktionen_2.js
@@ -5,8 +5,23 @@
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+X 4. Ausgabe in Konsole :
 */
+
+// MODULE: MULTIPLICATION a * b
+function mult(a, b) {
+  return a * b;
+}
+
+// MODULE: SUBTRACTION a - b
+function sub(a, b) {
+  return a - b;
+}
+
+// MODULE: ADDITION a + b
+function add(a, b) {
+  return a + b;
+}
 
 // MODULE:OUTPUT
 function output(outputData) {
