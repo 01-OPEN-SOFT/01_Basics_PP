39827d4477d2d86ac23e8710e1c92334419eceb7
Vladimir Haar <haar.vlad@gmx.de>
2025-01-13 07:45:19 +0000

    Arrays und forschleif fertig.

--- src/11_arrays_1.js
+++ src/11_arrays_1.js
@@ -7,7 +7,6 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
 //arr = new Array();  // Konstruktor
 // arr = []; // so geht auch
 // arr = [2, 11, 7, 8, 10];
@@ -36,29 +35,123 @@ let arr;
 // }
 
 /*** 01a. Funktionalität mit Array 1 */
-output(getSentenceArr(['Ich', 'bin', 'die', 'coole', 'Maxine', 'Mützerich']));
-function getSentenceArr(arr) {
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+
+// output(getSentenceArr(['Ich', 'bin', 'die', 'coole', 'Maxine', 'Mützerich']));
+// function getSentenceArr(arr) {
+//   const GAP = ' ';
+//   const PUNCT = '.';
+//   let str =
+//     arr[0] +
+//     GAP +
+//     arr[1] +
+//     GAP +
+//     arr[2] +
+//     GAP +
+//     arr[3] +
+//     GAP +
+//     arr[4] +
+//     GAP +
+//     arr[5] +
+//     PUNCT;
+
+//   return str;
+// }
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi");
+// function output(outputStr) {
+//   console.log(outputStr);
+// }
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+// for (let i = 0; i < 5; i++) {
+//   output('Index: ' + i);
+// }
+
+// for (let i = 10; i > 0; i--) {
+//   output('Index: ' + i);
+// }
+
+// // Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 5) {
+//   output(i);
+// }
+
+// let arr = ['Ich', 'bin', 'die', 'coole', 'Maxine', 'Mützerich'];
+// for (let i = 0; i < arr.length; i++) {
+//   //    output(i);
+//   output(arr[i]);
+// }
+
+// function output(outputStr) {
+//   console.log(outputStr);
+// }
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+// function output(outputStr) {
+//   console.log(outputStr);
+// }
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//   a += 1; // a = a + 1
+//   output('inhalt von a: ' + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    str += "Test" + " "; // a = a + 1
+//    output(str);
+// }
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+
+output(getSentenceArr2(['Ich', 'bin', 'die', 'coole', 'Maxine', 'Mützerich']));
+function getSentenceArr2(arr) {
   const GAP = ' ';
   const PUNCT = '.';
-  let str =
-    arr[0] +
-    GAP +
-    arr[1] +
-    GAP +
-    arr[2] +
-    GAP +
-    arr[3] +
-    GAP +
-    arr[4] +
-    GAP +
-    arr[5] +
-    PUNCT;
+  let str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i + GAP];
+  }
 
   return str;
 }
 
-// Modul: Ausgabe in Konsole : Test
-// output("hi");
 function output(outputStr) {
   console.log(outputStr);
 }
